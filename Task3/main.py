4d7a77a71ab228a3d664374560bb7c3d966dd320
oblivion4us <oblivion4us@icloud.com>
2024-10-18 00:58:18 +0200

    foldering

--- Task3/main.py
+++ Task3/main.py
@@ -5,27 +5,21 @@ from webdriver_manager.microsoft import EdgeChromiumDriverManager
 import time
 import csv
 
-# Установка и инициализация драйвера Edge
 driver = webdriver.Edge(service=Service(EdgeChromiumDriverManager().install()))
 
-# Открытие страницы
 driver.get('https://www.divan.ru/kaliningrad/category/divany-i-kresla')
-time.sleep(5)  # Ожидание загрузки страницы
+time.sleep(5)
 
-# Поиск элементов с ценами диванов
 divan_prices = driver.find_elements(By.XPATH, "//span[contains(@class, 'ui-LD-ZU KIkOH')]")
 
-# Открытие CSV файла для записи
 with open('prices.csv', mode='w', newline='', encoding='utf-8') as file:
     writer = csv.writer(file)
-    writer.writerow(['Price'])  # Записываем заголовок столбца
+    writer.writerow(['Price'])
 
-    # Записываем цены в CSV файл
-    if divan_prices:  # Проверяем, что список не пуст
+    if divan_prices:
         for price in divan_prices:
             writer.writerow([price.text])
     else:
         print("Цены не найдены.")
 
-# Закрытие браузера
 driver.quit()
